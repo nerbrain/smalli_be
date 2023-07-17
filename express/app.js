fe7be1714547b7abb3750c239d4d00725b0a5b57
nerbrain <malikmox@gmail.com>
2023-07-17 12:59:34 +0300

    more changes

--- express/app.js
+++ express/app.js
@@ -11,18 +11,18 @@ var urlsRouter = require('./routes/urls');
 var userRouter = require('./routes/users');
 
 
-var whitelist = ['http://localhost:3000','https://app.smalli.tech/']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions))
-// app.use(cors())
+// var whitelist = ['http://localhost:3000','https://app.smalli.tech/']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+// app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json());
 
 app.use('/', indexRouter);
